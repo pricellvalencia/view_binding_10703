c97e7d5ca1925837064116abfe72fb11d144e0ed
pricellvalencia <pricellvalencia2222@gmail.com>
2022-09-15 16:00:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10703/TaskList.kt
@@ -0,0 +1,17 @@
+package com.example.view_binding_10703
+
+object TaskList {
+    val tasklist = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur",
+            "Kebiasaan  sehari-hari untuk membuat hari makin indah"),
+        Task("Chapter 2", "Nguli Ghensin Impact",
+            "Mencari Primo buat Gatcha mama Yaemiko Tercinta"),
+        Task("Chapter 3", "Ngopi",
+            "Habis capek Nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "Dimana mana Ei?",
+            "Buat aduk Mekanik Showcase ayaka pyro"),
+        Task("Chapter 5", "Upload ke Tiktok",
+            "Buat Pamer ke view Bom Bakudan Klee C6 + Lisa C6"),
+
+        )
+}
